4388e75831f2ab1debf907490591570df17be396
Claudin Daniel <c.daniel@amigoexpress.com>
2018-05-22 13:47:16 -0400

    Added unit tests

---
+++ tests/ComposhipsTest.php
@@ -0,0 +1,94 @@
+<?php
+
+use Awobaz\Compoships\Database\Eloquent\Model;
+use Awobaz\Compoships\Tests\Model\Allocation;
+use Awobaz\Compoships\Tests\Model\TrackingTask;
+
+require_once __DIR__. '/TestCase.php';
+
+class ComposhipsTest extends TestCase
+{
+    /**
+     * Test the save method on a relationship
+     *
+     * @return void
+     */
+    public function testSave()
+    {
+        Model::unguard();
+
+        $allocation = new Allocation();
+        $allocation->booking_id = 1;
+        $allocation->vehicle_id = 1;
+        $allocation->save();
+
+        $allocation->trackingTasks()->save(new TrackingTask());
+
+        $this->assertNotNull($allocation->trackingTasks);
+
+        Model::unguard();
+    }
+
+    /**
+     * Test the save method on a relationship
+     *
+     * @return void
+     */
+    public function testSaveMany()
+    {
+        Model::unguard();
+
+        $allocation = new Allocation();
+        $allocation->booking_id = 1;
+        $allocation->vehicle_id = 1;
+        $allocation->save();
+
+        $allocation->trackingTasks()->saveMany([
+            new TrackingTask(),
+            new TrackingTask(),
+            new TrackingTask()
+        ]);
+
+        $this->assertNotNull($allocation->trackingTasks);
+        $this->assertEquals($allocation->trackingTasks->count(), 3);
+
+        Model::unguard();
+    }
+
+    /**
+     * Test the create method on a relationship
+     *
+     * @return void
+     */
+    public function testCreate()
+    {
+        Model::unguard();
+
+        $allocation = new Allocation();
+        $allocation->booking_id = 1;
+        $allocation->vehicle_id = 1;
+        $allocation->save();
+
+        $allocation->trackingTasks()->create([]);
+
+        $this->assertNotNull($allocation->trackingTasks);
+
+        Model::unguard();
+    }
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testWhereHas()
+    {
+        $allocations = Allocation::wherehas('trackingTasks')->get()->toArray();
+
+        $this->assertInternalType('array', $allocations);
+
+        $allocations = Allocation::query()->has('trackingTasks')->get()->toArray();
+
+        $this->assertInternalType('array', $allocations);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    /**
+     * Boots the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../vendor/laravel/laravel/bootstrap/app.php';
+
+        $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+
+        return $app;
+    }
+
+    /**
+     * Setup DB before each test.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->app['config']->set('database.default','sqlite');
+        $this->app['config']->set('database.connections.sqlite.database', ':memory:');
+
+        $this->migrate();
+    }
+
+    /**
+     * run package database migrations
+     *
+     * @return void
+     */
+    public function migrate()
+    {
+        $fileSystem = new Filesystem;
+        $fileSystem->requireOnce(__DIR__ . "/migrations.php");
+
+        (new Migration())->up();
+    }
+}
