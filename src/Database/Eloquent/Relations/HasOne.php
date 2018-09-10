85d35170b2f6a32f256354af082375b854c4ebe7
Fabian Mangelsdorf <fabian.mangelsdorf@haendlerbund.de>
2018-09-10 16:21:41 +0200

    extends from HasOne / HasMany instead of HasOneOrMany

--- src/Database/Eloquent/Relations/HasOne.php
+++ src/Database/Eloquent/Relations/HasOne.php
@@ -5,8 +5,9 @@ namespace Awobaz\Compoships\Database\Eloquent\Relations;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\Concerns\SupportsDefaultModels;
+use Illuminate\Database\Eloquent\Relations\HasMany as BaseHasMany;
 
-class HasOne extends HasOneOrMany
+class HasOne extends BaseHasMany
 {
     use SupportsDefaultModels;
 
