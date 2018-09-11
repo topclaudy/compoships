acedf85ea83ed0381bfd43631c19e97dc3ed2de2
Fabian Mangelsdorf <fabian.mangelsdorf@haendlerbund.de>
2018-09-11 17:47:53 +0200

    use HasOneOrMany as a trait, extends from Eloquent HasOne / HasMany

--- src/Database/Eloquent/Relations/HasOne.php
+++ src/Database/Eloquent/Relations/HasOne.php
@@ -5,10 +5,12 @@ namespace Awobaz\Compoships\Database\Eloquent\Relations;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\Concerns\SupportsDefaultModels;
+use Illuminate\Database\Eloquent\Relations\HasOne as BaseHasOne;
 
-class HasOne extends HasOneOrMany
+class HasOne extends BaseHasOne
 {
     use SupportsDefaultModels;
+    use HasOneOrMany;
 
     /**
      * Indicates if a default model instance should be used.
