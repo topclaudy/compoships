c46e4e2793701583e15d73919a30e3e6f2a63934
Vinícius Fagundes <fagundes.vinicius@eb.mil.br>
2023-08-31 10:35:16 -0300

    adapts for oracle/oci

--- src/Database/Query/Builder.php
+++ src/Database/Query/Builder.php
@@ -6,6 +6,41 @@ use Illuminate\Database\Query\Builder as BaseQueryBuilder;
 
 class Builder extends BaseQueryBuilder
 {
+    /**
+     * FROM: laravel-oci8
+     * Add a "where in" clause to the query.
+     * Split one WHERE IN clause into multiple clauses each
+     * with up to 1000 expressions to avoid ORA-01795.
+     *
+     * @param  string  $column
+     * @param  mixed  $values
+     * @param  string  $boolean
+     * @param  bool  $not
+     * @return \Illuminate\Database\Query\Builder
+     */
+    public function whereInOracle($column, $values, $boolean = 'and', $not = false)
+    {
+        $type = $not ? 'NotIn' : 'In';
+
+        if ($values instanceof Arrayable) {
+            $values = $values->toArray();
+        }
+
+        if (is_array($values) && count($values) > 1000) {
+            $chunks = array_chunk($values, 1000);
+
+            return $this->where(function ($query) use ($column, $chunks, $type, $not) {
+                foreach ($chunks as $ch) {
+                    $sqlClause = $not ? 'where'.$type : 'orWhere'.$type;
+                    $query->{$sqlClause}($column, $ch);
+                }
+            }, null, null, $boolean);
+        }
+
+        return parent::whereIn($column, $values, $boolean, $not);
+    }
+
+
     public function whereIn($column, $values, $boolean = 'and', $not = false)
     {
         //Here we implement custom support for multi-column 'IN'
@@ -25,7 +60,7 @@ class Builder extends BaseQueryBuilder
             return $this;
         }
 
-        return parent::whereIn($column, $values, $boolean, $not);
+        return $this->whereInOracle($column, $values, $boolean, $not);
     }
 
     public function whereColumn($first, $operator = null, $second = null, $boolean = 'and')
